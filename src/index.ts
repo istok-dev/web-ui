92938648048ce674ee59cd709a0364540134d284
ArtemAstakhov <artem_astakhov@yahoo.com>
2026-05-19 22:54:14 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,4 @@ export * from './components/tabs';
 export * from './components/tag';
 export * from './components/tooltip';
 export * from './components/toast';
-export * from './components/date-picker';
-export * from './components/calendar';
 export * from './components/field';
